1a5daed299a269f2976b7ecc37de3803fa372cae
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-05 16:18:37 +0500

    Directory Sync

---
+++ question1.dart
@@ -0,0 +1,9 @@
+void main() {
+  int length = 10;
+  int breadth = 5;
+  if (length == breadth) {
+    print("Selected Ranges Represent The shape is a square.");
+  } else {
+    print("Selected Ranges Represent The shape is a Rectangle");
+  }
+}
