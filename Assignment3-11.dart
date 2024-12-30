cf017f08d65825d1faf92aea1802b1df683b7d24
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-30 15:52:18 +0500

    Assignment 3 of Loops

---
+++ Assignment3-11.dart
@@ -0,0 +1,12 @@
+void main() {
+  int rows = 4;
+
+  for (int i = 1; i <= rows; i++) {
+    String pattern = '*';
+
+    for (int j = 1; j <= i; j++) {
+      pattern += '*';
+    }
+    print(pattern);
+  }
+}
